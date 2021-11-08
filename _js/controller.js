3be4af548da446e7b9cbbabe6a7f288ecc505821
JUAN CARLOS JIMENES JUNIOR <juan.carlos@solmarista.org.br>
2021-11-08 17:19:42 -0300

    organiza arquivos de projeto

---
+++ _js/controller.js
@@ -0,0 +1,30 @@
+//Aplica mascara no campo CPF
+
+let cpf = document.getElementById('cpf') .value;
+function mascaraCPF() {
+      if (cpf.value.lenght == 3) {
+
+      }
+}	
+//Iníco verificação de preenchimento de campos do formulário.
+function verificar(){
+
+/*
+var nome = document.getElementById("nome").value;
+var email = document.getElementById("email").value;
+var celular = document.getElementById("celular").value;
+var cpf = document.getElementById("cpf").value;
+var endereço = document.getElementById("endereco").value;
+var número = document.getElementById("numero").value;
+var bairro = document.getElementById("bairro").value;
+var complemento = document.getElementById("complemento").value;
+
+
+	if (nome){
+		alert("Campos não preenchidos, favor preenchê-los");
+	} else{
+		alert("Campos preenchidos com sucesso!");
+	}
+*/
+var nome = prompt("digite seu nome");
+}
